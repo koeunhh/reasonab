2abc997a8f9aea68720b9b1b1368c74ea005fcf1
Koeun Lee <koeunhh@gmail.com>
2020-01-30 03:52:18 -0500

    added q&a to main page. main page styling done

--- back-end/routes/content.js
+++ back-end/routes/content.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const router = express.Router();
 const main = require('../data/main');
+const questions = require('../data/questions');
 const about = require('../data/about');
 const program = require('../data/program');
 
 router.get('/main', (req, res) => {
   res.json(main);
+})
+
+router.get('/questions', (req, res) => {
+  res.json(questions);
 })
 
 router.get('/about', (req, res) => {
--- front-end/src/components/kor/MainKor.js
+++ front-end/src/components/kor/MainKor.js
@@ -7,25 +7,19 @@ export default class MainKor extends Component {
   constructor(props){
     super(props);
     this.state = {
-      data: []
+      data: [],
+      qa: null
     }
   }
 
   componentDidMount(){
     this.props.whiteBackground();
     this.getData();
+    this.getQuestions();
   }
 
-  toAbout = () => {
-    this.props.history.push('/en/about');
-  }
-
-  toProgram = () => {
-    this.props.history.push('/en/program');
-  }
-
-  toTrial = () => {
-    this.props.history.push('/en/trial');
+  toLink = (lang, page) => {
+    this.props.history.push(`/${lang}/${page}`);
   }
 
   getData = () => {
@@ -33,34 +27,73 @@ export default class MainKor extends Component {
     
     // axios.get('http://localhost:8080/content/main')
     axios.get('https://rda-toronto.herokuapp.com/content/main')
-    .then(res => {
-      this.setState({data: res.data[lang]});
-    })
-    .catch(err => {
-      console.log(err);
-    });
+      .then(res => {
+        this.setState({data: res.data[lang]});
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
+
+  getQuestions = () => {
+    const lang = this.props.checkLanguage();
+
+    // axios.get('http://localhost:8080/content/questions')
+    axios.get('https://rda-toronto.herokuapp.com/content/main/questions')
+      .then(res => {
+        this.setState({qa: res.data[lang]});
+      })
+      .catch(err => {
+        console.log(err);
+      })
   }
 
   render(){
-    const {data} = this.state;
+    const {data, qa} = this.state;
+    const lang = this.props.checkLanguage();
+
+    if(qa === null){
+      return false;
+    }
 
     return(
       <div className='main'>
+        <div className='main__nav'></div>
         <div className='main__logo'>
           <img src='/assets/icons/mainlogo-white.svg' alt='logo'/>
         </div>
         {data.map(each => 
           <div className={`main__${each.section}`} key={data.indexOf(each)}>
             <img className={`main__${each.section}--img`} src={each.img} alt={each.imgAlt}/>
-            <div>
+            <div className={`main__${each.section}--text`}>
               <h2>{each.title}</h2>
-              <h4>{each.sub}</h4>
-              <button onClick={this.toAbout}>{each.btn}</button>
+              <p>{each.sub}</p>
+              <button onClick={() => this.toLink(lang, each.section)}>{each.btn}</button>
             </div>
           </div>  
         )} 
-        <img src='/assets/images/mainImg4.svg' alt='mainImg4'/>
-        <h2>Welcome to RDA</h2>   
+        <div className='main__welcome'>
+          <img src='/assets/images/mainImg4.svg' alt='mainImg4'/>
+          <h2>Welcome to RDA</h2>   
+        </div>
+        <div className='main__qa'>
+          <div className='main__qa--title'>
+            <h2>{qa.title}</h2>
+            <img src='/assets/images/mainImg5.svg' alt='questionAnswer'/>
+          </div>
+          <div className='main__qa--questions'>
+            {qa.questions.map(each => 
+              <div className='qa-each' id={`q${each.id}`} key={each.id}>
+                <h3>{`${each.id}. ${each.question}`}</h3>
+                <p>{each.answer}</p>
+              </div>
+            )}
+            <div id='qa-more'>
+              <p>{qa.more}</p>
+              <button>{qa.btn}</button>
+            </div>
+          </div>
+        </div>
       </div>
     )
   }
